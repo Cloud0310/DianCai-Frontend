ac1e23a6e90cf49ea26d0dedb920f160bb5363df
Hananoisakura <122717517+Hananoisakura@users.noreply.github.com>
2023-08-15 15:53:40 +0800

    新增form和music里的内容

--- src/components/Form.vue
+++ src/components/Form.vue
@@ -1,18 +1,18 @@
 <template>
   <form action="/upload" id="uploadForm" enctype="image/*" method="post">
-    <p>
+    <li>
       <label for="file" style="font-weight: bold">添加图片：</label>
       <input type="file" name="file" id="file" />
-    </p>
-    <p>
+    </li>
+    <li>
       <a style="font-weight: bold">生成模型：</a>
       <select name="mode" id="select_2" tabindex="0">
         <option value="0">测试用</option>
         <option value="1">Mubert</option>
         <option value="2">Riffusion</option>
       </select>
-    </p>
-    <p>
+    </li>
+    <li>
       <a style="font-weight: bold">您是否有期待的情感色彩：</a>
       <select name="emo" id="select_1" tabindex="1">
         <option value="1">none</option>
@@ -22,6 +22,6 @@
         <option value="5">sadness</option>
         <option value="6">amusement</option>
       </select>
-    </p>
+    </li>
   </form>
 </template>
